05318aa6560cf272bd423d5dc189bd9bd5abeaba
Morgan-Mischo <goramorgan@gmail.com>
2019-07-09 12:46:30 -0600

    actually finished now

--- server/messagesCtrl.js
+++ server/messagesCtrl.js
@@ -7,7 +7,17 @@ module.exports = {
     createMessage(req, res) {
         let { username, message } = req.body; 
         let newMessage = { username, message };
+        if(req.session.history) {
+            req.session.history.push(newMessage)
+        }
+        else {
+            req.session.history = []
+            req.session.history.push(newMessage)
+        }
         allMessages.push(newMessage);
         res.status(200).send(allMessages);
+    },
+    history(req, res) {
+        res.status(200).send(req.session.history)
     }
 };
